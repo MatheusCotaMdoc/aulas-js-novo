99f515ec9e7ed3ccac89ede21ff59b987d829b86
MATHEUS DAVI DE OLIVEIRA COTA <matheus.docota@senacsp.edu.br>
2024-07-10 11:50:58 -0300

    atualizações

---
+++ exercicio-aula6/aula6.js
@@ -0,0 +1,8 @@
+function somarValores(){
+    let n1 = Number(document.querySelector('#n1').value)
+    let n2 = Number(document.querySelector('#n2').value) 
+
+   
+
+    document.querySelector('#resultado').innerHTML = n1+n2
+}
